1a4fbaf610a0dcaf82e21dca2b29b43c65543c28
ananya-1307 <71624430+ananya-1307@users.noreply.github.com>
2022-05-30 01:03:13 +0530

    Update Best TimetoBuyandSellStock.java

---
+++ Arrays/Best TimetoBuyandSellStock.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxProfit(int[] arr) {
+        int min=Integer.MAX_VALUE;
+        int max=0;
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]<min)
+            {
+                min=arr[i];
+            }
+            else if(arr[i]-min>max)
+            {
+                max=arr[i]-min;
+                
+
+            }
+        }
+        return max;
+    }
+}
